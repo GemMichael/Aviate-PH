0efc63b763aa2d78ed6be30d1756b992896b84dc
GemMichael <mhigenieva@gmail.com>
2024-11-05 09:24:45 +0800

    fixing video

--- src/home/PilotJourney.jsx
+++ src/home/PilotJourney.jsx
@@ -19,62 +19,102 @@ function Pilotjourney() {
 
     useEffect(() => {
         const handleResize = () => {
-            setIsMobile(window.innerWidth <= 768); 
+            setIsMobile(window.innerWidth <= 768);
         };
         window.addEventListener('resize', handleResize);
         handleResize();
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
+
     const videoDetails = [
-        { src: captjm, title: '200hrs', description: 'more here' },
-        { src: captjmpinning, title: 'Pinning', description: 'more here' },
-        { src: captstef, title: 'First Solo', description: 'more here' },
-        { src: pilotmatts, title: 'Cross Country', description: 'more here' },
-        { src: captjcrage, title: 'Pilot Journey', description: 'more here' },
+        {
+            src: captjm,
+            title: '200hrs',
+            description: 'more here',
+        },
+        {
+            src: captjmpinning,
+            title: 'pinning',
+            description: 'more here',
+        },
+        {
+            src: captstef,
+            title: 'First solo',
+            description: 'more here',
+        },
+        {
+            src: pilotmatts,
+            title: 'Cross Country',
+            description: 'more here',
+        },
+        {
+            src: captjcrage,
+            title: 'Pilot journey',
+            description: 'more here',
+        },
     ];
 
     const handleVideoClick = (videoSrc, index) => {
         regularVideoRefs.current.forEach((video, idx) => {
-            if (video && idx !== index) video.pause();
+            if (video && idx !== index) {
+                video.pause();
+            }
         });
+
+
         setEnlargedVideo(videoSrc);
         setVideoContent(videoDetails[index]);
+
+
         setTimeout(() => {
-            if (videoRef.current) videoRef.current.play();
+            if (videoRef.current) {
+                videoRef.current.play();
+            }
         }, 0);
     };
 
     const handleClose = () => {
-        if (videoRef.current) videoRef.current.pause();
+        if (videoRef.current) {
+            videoRef.current.pause();
+        }
         setEnlargedVideo(null);
         setVideoContent({ title: '', description: '' });
     };
 
     const handleEnlargedVideoPlay = () => {
         regularVideoRefs.current.forEach((video) => {
-            if (video) video.pause();
+            if (video) {
+                video.pause();
+            }
         });
     };
 
     return (
         <section className="max-w-[1160px] w-full mx-auto mt-[168px] px-[24px] lg:px-0">
-            <h3 className="text-[56px] text-center font-bold text-black">Pilot Journeys</h3>
+            <h3 className="text-[56px] justify-center text-center font-bold text-black leading-[1]">Pilot Journeys</h3>
             <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4 mt-[51px]">
                 <div className="grid gap-4">
                     <video
                         className="h-full w-full object-cover cursor-pointer rounded-lg"
                         src={captjm}
                         controls
-                        autoPlay={!isMobile} 
+                        autoPlay={!isMobile}
                         muted
                         loop
                         ref={(el) => (regularVideoRefs.current[0] = el)}
                         onClick={() => handleVideoClick(captjm, 0)}
                     />
-                    <a href="#" className="overflow-hidden rounded-lg" data-aos="fade-up" data-aos-duration="1000">
-                        <img className="h-full w-full object-cover transform hover:scale-150 transition-all duration-600" src={pic1} alt="" />
-                    </a>
+                    <video
+                        className="h-full w-full rounded-lg object-cover cursor-pointer"
+                        src={captstef}
+                        controls
+                        autoPlay={!isMobile}
+                        muted
+                        loop
+                        ref={(el) => (regularVideoRefs.current[4] = el)}
+                        onClick={() => handleVideoClick(captstef, 2)}
+                    />
                     <video
                         className="h-full w-full object-cover cursor-pointer rounded-lg"
                         src={pilotmatts}
@@ -86,12 +126,78 @@ function Pilotjourney() {
                         onClick={() => handleVideoClick(pilotmatts, 3)}
                     />
                 </div>
-              
+
+                <div className="grid gap-4">
+                    <video
+                        className="h-full w-full object-cover cursor-pointer rounded-lg"
+                        src={captjmpinning}
+                        controls
+                        autoPlay={!isMobile}
+                        muted
+                        loop
+                        ref={(el) => (regularVideoRefs.current[2] = el)}
+                        onClick={() => handleVideoClick(captjmpinning, 1)}
+                    />
+                    <video
+                        className="h-full w-full object-cover cursor-pointer rounded-lg"
+                        src={captjmpinning}
+                        controls
+                        autoPlay={!isMobile}
+                        muted
+                        loop
+                        ref={(el) => (regularVideoRefs.current[2] = el)}
+                        onClick={() => handleVideoClick(captjmpinning, 1)}
+                    />
+                    <video
+                        className="h-full w-full object-cover cursor-pointer rounded-lg"
+                        src={captjmpinning}
+                        controls
+                        autoPlay={!isMobile}
+                        muted
+                        loop
+                        ref={(el) => (regularVideoRefs.current[2] = el)}
+                        onClick={() => handleVideoClick(captjmpinning, 1)}
+                    />
+
+                </div>
+
+                <div className="grid gap-4">
+                    <video
+                        className="h-full w-full rounded-lg object-cover cursor-pointer"
+                        src={captstef}
+                        controls
+                        autoPlay={!isMobile}
+                        muted
+                        loop
+                        ref={(el) => (regularVideoRefs.current[4] = el)}
+                        onClick={() => handleVideoClick(captstef, 2)}
+                    />
+                    <video
+                        className="h-full w-full object-cover cursor-pointer rounded-lg"
+                        src={captjcrage}
+                        controls
+                        autoPlay={!isMobile}
+                        muted
+                        loop
+                        ref={(el) => (regularVideoRefs.current[3] = el)}
+                        onClick={() => handleVideoClick(captjcrage, 4)}
+                    />
+                    <video
+                        className="h-full w-full rounded-lg object-cover cursor-pointer"
+                        src={captstef}
+                        controls
+                        autoPlay={!isMobile}
+                        muted
+                        loop
+                        ref={(el) => (regularVideoRefs.current[4] = el)}
+                        onClick={() => handleVideoClick(captstef, 2)}
+                    />
+                </div>
             </div>
 
             {enlargedVideo && (
                 <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
-                    <div className="relative flex flex-col items-center">
+                    <div className="relative flex flex-col justify-center items-center">
                         <video
                             ref={videoRef}
                             className="h-[80vh] w-auto object-cover cursor-pointer rounded-lg"
@@ -99,9 +205,14 @@ function Pilotjourney() {
                             controls
                             onPlay={handleEnlargedVideoPlay}
                         />
-                        <button className="absolute top-4 right-4 text-white text-2xl" onClick={handleClose}>
+
+                        <button
+                            className="absolute top-4 right-4 text-white text-2xl"
+                            onClick={handleClose}
+                        >
                             &times;
                         </button>
+
                         <div className="mt-4 text-white text-center">
                             <h3 className="text-2xl font-bold">{videoContent.title}</h3>
                             <p>{videoContent.description}</p>
